1150459e65fe1384fa4e677db4c052612498b41a
ilkhoeri <khoeriilham@gmail.com>
2025-02-04 21:36:01 +0700

    into [v0.0.45]

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,9 @@
  *
  * @template T - The input type, which should ideally be a function.
  * @returns The inferred return type of the function if `T` is a function type; otherwise, `never`.
- *
+ * @example
+ * const MyFunction = () => {};
+ * type Result = inferType<typeof MyFunction>; // type Result = void.
  * @see {@link https://ilkhoeri.github.io/cretex/types#infertype Docs}
  */
 export type inferType<T> = T extends (...args: any[]) => infer R ? R : never;
